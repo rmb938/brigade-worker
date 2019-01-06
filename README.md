cccaa179ea093a3e23a314ea84ad0ba7079926c6
Ryan Belgrave <ryan@belgrave.me>
2019-01-06 17:00:30 -0600

    don't be complex

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # brigade-worker
 Brigade Worker Image with helper libraries 
+
+## sops
+
+Decrypt (sops)[https://github.com/mozilla/sops]files 
+
+```javascript
+const { SopsJob } = require("./sops");
+
+events.on("exec", (e, p) => {
+  const decryptedPath = '/mnt/sops/decrypted';
+
+  // /src/encrypted is a directory filled with sops secrets
+  const sopsJob = new SopsJob("/src/encrypted", decryptedPath);
+
+  // persist the secrets so we can use them in other jobs
+  sopsJob.storage = {
+    enabled: true,
+    path: decryptedPath
+  };
+
+  // Load credentials, in this case a GCP service account
+  this.env["GCP_SERVICE_ACCOUNT_KEY"] = {
+    secretKeyRef: {
+      name: "sops",
+      key: "gcp"
+    }
+  };
+
+  // sops uses the default GCP credentials
+  this.env["GOOGLE_APPLICATION_CREDENTIALS"] = '/src/gcp_creds.json';
+
+  // GCP creds need to be base64 so the environment doesn't get weird and the need to be placed into a file
+  sopsJob.tasks.unshift("echo ${GCP_SERVICE_ACCOUNT_KEY} | base64 -d > ${GOOGLE_APPLICATION_CREDENTIALS}");
+
+  sopsJob.run();
+});
+```
--- sops.js
+++ sops.js
@@ -1,39 +1,18 @@
 const { events, Job, Group } = require("brigadier");
-const fs = require('fs');
 
 class SopsJob extends Job {
-  constructor(encryptedPath = "encrypted", decryptedPath = "decrypted") {
+  constructor(encryptedPath, decryptedPath) {
     super("sops", "mozilla/sops:latest");
 
-    this.storage = {
-      enabled = true,
-      path = decryptedPath
-    }
-
-    this.env = {
-      GCP_SERVICE_ACCOUNT_KEY: {
-        secretKeyRef: {
-          name: "sops",
-          key: "gcp"
-        }
-      }
-    }
-
-    vcsEncryptedPath = `/vcs/${encryptedPath}`
-    vcsDecryptedPath = `/vcs/${decryptedPath}`
-
-    srcEncryptedPath = `/src/${encryptedPath}`
-    srcDecryptedPath = `/src/${decryptedPath}`
-
     this.tasks = [
+      `set -o pipefail`,
       `set -x`,
-      `ls -la ${srcEncryptedPath}`
+      `ls -la ${encryptedPath}`,
+      `for filepath in ${encryptedPath}/*; do`,
+      'filename=`basename $filepath`',
+      `sops -d ${encryptedPath}/$filename > ${decryptedPath}/$filename`,
+      `done`,
+      `ls -la ${decryptedPath}`
     ];
-
-    fs.readdirSync(encryptedPath).forEach(file => {
-      this.tasks.push(`sops -d ${srcDecryptedPath}/${file} > ${srcDecryptedPath}/${file}`)
-    })
-
-    this.tasks.push(`ls -la ${srcDecryptedPath}`)
   }
 }
