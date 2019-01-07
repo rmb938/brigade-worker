5789e57404434e77c70f810d7f55781d8f8c8064
Ryan Belgrave <ryan@belgrave.me>
2019-01-07 08:42:27 -0600

    well extending didn't work so go back to basics

--- sops.js
+++ sops.js
@@ -1,18 +1,18 @@
 const { Job } = require("./brigadier");
 
-export class SopsJob extends Job {
-  constructor(encryptedPath, decryptedPath) {
-    super("sops", "mozilla/sops:latest");
+exports.SopsJob = function (encryptedPath, decryptedPath) {
+  j = new Job("sops", "mozilla/sops:latest")
 
-    this.tasks = [
-      'set -o pipefail',
-      'set -x',
-      `ls -la ${encryptedPath}`,
-      `for filepath in ${encryptedPath}/*; do`,
-      'filename=`basename $filepath`',
-      `sops -d ${encryptedPath}/$filename > ${decryptedPath}/$filename`,
-      'done',
-      `ls -la ${decryptedPath}`
-    ];
-  }
+  j.tasks = [
+    'set -o pipefail',
+    'set -x',
+    `ls -la ${encryptedPath}`,
+    `for filepath in ${encryptedPath}/*; do`,
+    'filename=`basename $filepath`',
+    `sops -d ${encryptedPath}/$filename > ${decryptedPath}/$filename`,
+    'done',
+    `ls -la ${decryptedPath}`
+  ];
+
+  return j
 }
