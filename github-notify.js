5d7a7a869bff6eb4b8149e71638428bf160a3a39
Ryan Belgrave <ryan@belgrave.me>
2019-01-10 09:14:41 -0600

    notify don't include commit

--- github-notify.js
+++ github-notify.js
@@ -26,8 +26,6 @@ exports.Notify = class Notify {
 
       // We get the token from the project's secrets section.
       GH_TOKEN: this.proj.secrets.ghToken, // YOU MUST SET THIS IN YOUR PROJECT
-
-      GH_COMMIT: this.event.commit,
     };
     return j.run();
   }
